c67590a55c641708da2dab5efddc7d07b4ef0bca
Guo Haipeng <hguo@admins-MacBook-Pro.local>
2024-10-02 01:22:17 +0300

    add send GET&POST interchangably into stress test

--- stress_test.go
+++ stress_test.go
@@ -18,6 +18,7 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/assert"
+	"golang.org/x/exp/rand"
 )
 
 var numClients = constants.QUEUE_CAPACITY
@@ -48,6 +49,7 @@ func TestMainStess(t *testing.T) {
 	go utils.StartServer(config, stopChan)
 
 	t.Run("stress testing, multiple POST&GET /receipts request", func(t *testing.T) {
+
 		waitTime := numClients * 1
 		var wg sync.WaitGroup
 		var mu sync.Mutex
@@ -169,5 +171,93 @@ func TestMainStess(t *testing.T) {
 			}(userToken, uploadResp.ReceiptID)
 		}
 		wg.Wait()
+	})
+
+	t.Run("stress testing, multiple POST and GET inter-changeably", func(t *testing.T) {
+
+		// to prepare test, upload 10 images sequentialy
+		receiptIDs := make(map[string]string)
+		url := baseUrl + "/receipts"
+		size := "medium"
+		numRequests := constants.QUEUE_CAPACITY
+		var wg sync.WaitGroup
+
+		for i := 0; i < numRequests; i++ {
+			userToken := "inter_user_token_" + strconv.Itoa(i)
+			req, reqErr := test_utils.GenerateUploadRequest(t, url, "test_image.jpg", userToken)
+			assert.Nil(t, reqErr)
+
+			resp, err := client.Do(req)
+			assert.Nil(t, err)
+			defer resp.Body.Close()
+
+			assert.Equal(t, http.StatusCreated, resp.StatusCode)
+
+			var uploadResp http_responses.UploadResponse
+			test_utils.ParseResponseBody(t, resp, &uploadResp)
+			assert.NotEmpty(t, uploadResp.ReceiptID)
+			receiptIDs[userToken] = uploadResp.ReceiptID
+		}
+
+		// generate GET and POST requests
+		reqs := []*http.Request{}
+		methods := []string{http.MethodGet, http.MethodPost}
+
+		for i := 0; i < numRequests; i++ {
+			rand.Seed(uint64(time.Now().UnixNano()))
+			randomInt := rand.Intn(2)
+
+			method := methods[randomInt]
+			if method == http.MethodGet {
+				userToken := "inter_user_token_" + strconv.Itoa(i)
+				receiptID := receiptIDs[userToken]
+				getUrl := fmt.Sprintf("%s/%s?size=%s", url, receiptID, size)
+				getReq, getReqErr := http.NewRequest(http.MethodGet, getUrl, nil)
+				assert.Nil(t, getReqErr)
+				getReq.Header.Set("username_token", userToken)
+				reqs = append(reqs, getReq)
+			}
+
+			if method == http.MethodPost {
+				postUserToken := "inter_post_user_token_" + strconv.Itoa(i)
+				postReq, postReqErr := test_utils.GenerateUploadRequest(t, url, "test_image.jpg", postUserToken)
+				assert.Nil(t, postReqErr)
+				reqs = append(reqs, postReq)
+			}
+		}
+
+		time.Sleep(time.Duration(numRequests) * time.Second)
+
+		// send request interchagnabley at same time
+		for _, req := range reqs {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+
+				resp, getErr := client.Do(req)
+				assert.Nil(t, getErr)
+				defer resp.Body.Close()
+
+				if req.Method == http.MethodGet {
+					assert.Equal(t, http.StatusOK, resp.StatusCode)
+					header, headerErr := test_utils.ParseDownloadResponseHeader(resp)
+					assert.Nil(t, headerErr)
+					assert.Equal(t, "image/jpeg", header.ContentType)
+
+					_, height := test_utils.GetImageDimension(t, resp)
+					assert.Equal(t, configs.AllowedDimensions[1].Height, height)
+				}
+
+				if req.Method == http.MethodPost {
+					assert.Equal(t, http.StatusCreated, resp.StatusCode)
+
+					var uploadResp http_responses.UploadResponse
+					test_utils.ParseResponseBody(t, resp, &uploadResp)
+					assert.NotEmpty(t, uploadResp.ReceiptID)
+				}
+			}()
+		}
+		wg.Wait()
+
 	})
 }
